7e9471054610f547b4943d3a6dd850366e0b5a93
Яцишин Никита <yatsishin.nik@mail.ru>
2026-02-20 15:20:04 +0300

    Программа считывания имени и вывода приветствия

---
+++ HelloName.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HelloName {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        String name = scan.next();
+        System.out.println("Привет, " + name + "!");
+        
+        scan.close();  
+    }
+}
